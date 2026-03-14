b7ed6ca019ab0855774c30ba23271ccef3686552
Collytics <collytics@kreski.pl>
2026-03-14 17:30:56 +0000

    style: global colors, Barlow font, button hover, nav, spacing

--- wp-content/themes/flatsome-child/functions.php
+++ wp-content/themes/flatsome-child/functions.php
@@ -1,10 +1,8 @@
 <?php
 /**
  * Flatsome Child — Kreski
- * functions.php
  */
 
-// Załaduj style rodzica (Flatsome) + child theme
 add_action( 'wp_enqueue_scripts', 'flatsome_child_enqueue_styles' );
 function flatsome_child_enqueue_styles() {
     wp_enqueue_style(
@@ -17,4 +15,17 @@ function flatsome_child_enqueue_styles() {
         array( 'flatsome-parent-style' ),
         wp_get_theme()->get( 'Version' )
     );
+    wp_enqueue_style(
+        'kreski-fonts',
+        'https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400&family=Barlow+Condensed:wght@600;700;800&display=swap',
+        array(),
+        null
+    );
+}
+
+// Preconnect Google Fonts dla wydajności
+add_action( 'wp_head', 'kreski_preconnect_fonts', 1 );
+function kreski_preconnect_fonts() {
+    echo '<link rel="preconnect" href="https://fonts.googleapis.com">' . "\n";
+    echo '<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>' . "\n";
 }
